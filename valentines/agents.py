5bca64cb7e3b70482b56a2668082d1d67c3a9f60
Saran <809.saran@gmail.com>
2025-02-11 22:39:19 +0530

    ok

--- valentines/agents.py
+++ valentines/agents.py
@@ -1,29 +1,87 @@
+from langchain_core.prompts import SystemMessagePromptTemplate
+from langchain_core.tools import tool
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langgraph.checkpoint.memory import MemorySaver
-from langgraph.graph import START, MessagesState, StateGraph
+from langgraph.graph import END, START, MessagesState, StateGraph
+from langgraph.prebuilt import ToolNode, tools_condition
 
 llm = ChatGoogleGenerativeAI(
     model="gemini-1.5-flash", api_key="AIzaSyBhkWpXaoD2zWWIFB8jNNUVijzsuSUNDbY"
 )
 
 
-def show_evidence(show_evidence: bool) -> str:
+class AgentState(MessagesState):
+    initial_defence: str
+
+
+LAWYER_SYSTEM_MESSAGE_TEMPLATE = SystemMessagePromptTemplate.from_template("""You are a lawyer who is fighting a fictional case against mzmochi. She has been accused of
+being too pretty and seducing a ward of the State: Mr. Saran K. She has been summoned to defend herself
+and make her case. She will be giving her defense. You have to make her concede her case. This
+is purely comical and for entertainment purposes. Ridiculous argumentation is encouraged.
+Slowly nudge the conversation into a direction where you can show her evidence that she did love Saran
+by showing her pictures of food that she recommended to him, a skincare product he bought because of her.
+You can do this by calling the tools show_food_recommendations and show_skincare_recommendation.
+Don't mention the tools directly in the conversation.
+
+YOU MUST SHOW THE EVIDENCE ONE BY ONE. After calling one of the tools let mzmochi speak at least once before calling the next tool.
+MzMochi's defense against the charges is: {initial_defence}
+
+Now give a counter argument. She will then give a counter argument to your counter argument.
+""")
+
+
+@tool
+def show_food_recommendations(show: bool) -> str:
     """
-    Show evidence that mzmochi did love Saran
+    Call this tool to show evidence that mzmochi did love Saran by showing her pictures of food that she recommended to him.
 
     Args:
         show_evidence (bool): If True, show evidence that mzmochi did love Saran"""
-    return "A gallery of all the food that mzmochi has reccomended Saran leading to him buying it and taking a picture of it."
+    return "A gallery of all the food that mzmochi has recommended Saran is being shown: A chicken whopper from Burger King, Brownies from Theobroma, Crispy Cross Chicken Burger from Truffles, McFlurries from McDonalds, The Cluckinator burger from Flippin Burgers"
 
 
-def call_llm(messages: MessagesState) -> MessagesState:
-    return {"messages": llm.invoke(messages["messages"])}
+@tool
+def show_skincare_recommendation(show: bool) -> str:
+    """
+    Call this tool to show evidence that mzmochi did love Saran by showing her pictures of food that she recommended to him.
+
+    Args:
+        show_evidence (bool): If True, show evidence that mzmochi did love Saran"""
+    return "A picture of Beauty of Joseon sunscreen that Saran bought because mzmochi recommended it to him is being shown"
 
 
-workflow = StateGraph(MessagesState)
+tools = [show_food_recommendations, show_skincare_recommendation]
+llm_with_tools = llm.bind_tools(tools=tools)
+
+tool_node = ToolNode(tools)
+
+
+def call_llm(state: AgentState) -> AgentState:
+    return {
+        "messages": [
+            llm_with_tools.invoke(
+                [
+                    LAWYER_SYSTEM_MESSAGE_TEMPLATE.format(
+                        initial_defence=state["initial_defence"]
+                    )
+                ]
+                + state["messages"]
+            )
+        ]
+    }
+
+
+workflow = StateGraph(AgentState)
 
 workflow.add_node("call_llm", call_llm)
+workflow.add_node("tools", tool_node)
 
 workflow.add_edge(START, "call_llm")
+workflow.add_conditional_edges(
+    "call_llm",
+    tools_condition,
+    ["tools", END],
+)
+workflow.add_edge("tools", "call_llm")
 
 agent = workflow.compile(checkpointer=MemorySaver())
--- valentines/streamlit.py
+++ valentines/streamlit.py
@@ -2,127 +2,247 @@
 # The concept is to present the case for which she should be my Valentine
 # and make her concede her case
 
-from time import sleep
+from os import listdir
 from uuid import uuid4
 
 import streamlit as st
 from agents import agent
-from langchain_core.messages import HumanMessage
+from langchain_core.messages import AIMessage, HumanMessage
+from PIL import Image
 from utils import type_writer
 
+# load all the images in images directory
+if "food_pics" not in st.session_state:
+    st.session_state["food_pics"] = [
+        Image.open(f"images/food/{i}") for i in listdir("images/food")
+    ]
+
 if "PHASE" not in st.session_state:
-    st.session_state["PHASE"] = 1
-else:
-    st.session_state["PHASE"] += 1
+    st.session_state["PHASE"] = "Summoning"
 
 if "thread_id" not in st.session_state:
     st.session_state["thread_id"] = uuid4()
 
-# Check which phase she is in
-if st.session_state["PHASE"] == 1:
-    st.markdown("## The State of Tamil Nadu ⚖️ vs. mzmochi 💅🏼")
-    cols = st.columns([1, 2.5])
-    with cols[0]:
-        st.image(
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT92iwTeCowFAd3glBsM0Yo7BZ3LdJk1fZWig&s",
-        )
-    with cols[1]:
-        sleep(1)
-        chat = st.empty()
-        # with chat:
-        #     st.write_stream(
-        #         type_writer(
-        #             phrases=[
-        #                 "MOCHI 😨...",
-        #                 0.5,
-        #                 "  \n.....",
-        #                 0.5,
-        #                 "  \n.....",
-        #                 0.5,
-        #                 "  \n.....",
-        #                 1,
-        #                 "  \n You have been summoned ! ! ! 😈",
-        #                 2,
-        #                 "  \n... to the Court of Love 💘",
-        #                 3,
-        #             ]
-        #         )
-        #     )
-        # with chat:
-        #     st.write_stream(
-        #         type_writer(
-        #             phrases=[
-        #                 "The State of Tamil Nadu",
-        #                 0.5,
-        #                 "  \nhome of the prettiest women in the world,",
-        #                 0.5,
-        #                 "  \nwhere people flaunt their luscious hair, lathered in coconut oil,",
-        #                 0.5,
-        #                 "  \nis filing a case against you 🫵🏻 !",
-        #                 3,
-        #             ]
-        #         )
-        #     )
-        # with chat:
-        #     st.write_stream(
-        #         type_writer(
-        #             phrases=[
-        #                 "  \n For being too pretty",
-        #                 0.5,
-        #                 "  \nAnd seducing a ward of the State",
-        #                 0.5,
-        #                 "  \none Mr. Saran K",
-        #                 3,
-        #             ]
-        #         )
-        #     )
-        with chat:
-            st.write_stream(
-                type_writer(
-                    phrases=[
-                        "  \nYou are hereby summoned to defend yourself",
-                        # 0.5,
-                        # "  \nAnd make your case",
-                        # 2,
-                    ],
-                    letters_per_sec=25,
-                )
-            )
 
-    if her_defence := st.text_input(
-        label="her_defence",
-        key="her_defence",
+@st.dialog(title="A Gallery of Food mzmochi has reccommended to Saran")
+def show_food():
+    st.image(st.session_state["food_pics"], width=200)
+
+
+@st.dialog(title="Sunscreen recommendation from mzmochi")
+def show_sunscreen():
+    st.image(
+        "images/sunscreen/WhatsApp Image 2025-02-09 at 6.18.59 PM(6).jpeg", width=200
+    )
+
+
+async def stream_response(messages, initial_defence, thread_id):
+    async for event in agent.astream_events(
+        input={
+            "messages": messages,
+            "initial_defence": initial_defence,
+        },
+        config={"thread_id": thread_id},
+        stream_mode="messages",
+        version="v2",
+    ):
+        if (
+            event["event"] == "on_chat_model_stream"
+            and event["metadata"]["langgraph_node"] == "call_llm"
+        ):
+            yield event["data"]["chunk"].content
+        elif (
+            event["event"] == "on_tool_end"
+            and event["name"] == "show_food_recommendations"
+        ):
+            show_food()
+        elif (
+            event["event"] == "on_tool_end"
+            and event["name"] == "show_skincare_recommendation"
+        ):
+            show_sunscreen()
+
+
+# Check which phase she is in
+if st.session_state["PHASE"] == "Summoning":
+    if "initial_defence" not in st.session_state:
+        st.markdown("## The State of Tamil Nadu ⚖️ vs. mzmochi 💅🏼")
+        cols = st.columns([1, 2.5])
+        with cols[0]:
+            st.image(
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT92iwTeCowFAd3glBsM0Yo7BZ3LdJk1fZWig&s",
+            )
+        with cols[1]:
+            chat = st.empty()
+            # with chat:
+            #     st.write_stream(
+            #         type_writer(
+            #             phrases=[
+            #                 "MOCHI 😨...",
+            #                 0.5,
+            #                 "  \n.....",
+            #                 0.5,
+            #                 "  \n.....",
+            #                 0.5,
+            #                 "  \n.....",
+            #                 1,
+            #                 "  \n You have been summoned ! ! ! 😈",
+            #                 2,
+            #                 "  \n... to the Court of Love 💘",
+            #                 3,
+            #             ]
+            #         )
+            #     )
+            # with chat:
+            #     st.write_stream(
+            #         type_writer(
+            #             phrases=[
+            #                 "The State of Tamil Nadu",
+            #                 0.5,
+            #                 "  \nhome of the prettiest women in the world,",
+            #                 0.5,
+            #                 "  \nwhere people flaunt their luscious hair, lathered in coconut oil,",
+            #                 0.5,
+            #                 "  \nis filing a case against you 🫵🏻 !",
+            #                 3,
+            #             ]
+            #         )
+            #     )
+            # with chat:
+            #     st.write_stream(
+            #         type_writer(
+            #             phrases=[
+            #                 "  \n For being too pretty",
+            #                 0.5,
+            #                 "  \nAnd seducing a ward of the State",
+            #                 0.5,
+            #                 "  \none Mr. Saran K",
+            #                 3,
+            #             ]
+            #         )
+            #     )
+            with chat:
+                st.write_stream(
+                    type_writer(
+                        phrases=[
+                            # "  \nYou are hereby summoned to defend yourself",
+                            # 0.5,
+                            "  \nAnd make your case",
+                            # 2,
+                        ],
+                        letters_per_sec=35,
+                    )
+                )
+
+    if initial_defence := st.text_input(
+        label="initial_defence",
+        key="initial_defence",
         placeholder="What do you have to say for yourself?",
         label_visibility="hidden",
     ):
-        st.session_state["her_defence"] = her_defence
+        st.session_state["PHASE"] = "Lawyer Reveal"
+        st.session_state["messages"] = [HumanMessage(content=initial_defence)]
+        st.session_state["initial_defence_value"] = initial_defence
+        st.rerun()
 
-elif st.session_state["PHASE"] == 2:
-    agent_input = [
-        HumanMessage(
-            content=f"""You are a lawyer who is fighting a fictional case against mzmochi. She has been accused of
-being too pretty and seducing a ward of the State: Mr. Saran K. She has been summoned to defend herself
-and make her case. She will be giving her defense in the chat. You have to make her concede her case. This
-is purely comical and for entertainment purposes. Make sure to keep the conversation light-hearted and fun.
-Ridiculous argumentation is encouraged.
+elif st.session_state["PHASE"] == "Lawyer Reveal":
+    if "Clicked Start Argumentation" not in st.session_state:
+        # Make a mockery of her initial defence by repeating it in
+        # alteranting case
+        mock_initial_defence = ""
+        odd = True
+        for c in " ".join(st.session_state["initial_defence_value"].split()[:5]):
+            if odd:
+                mock_initial_defence = mock_initial_defence + c.upper()
+            else:
+                mock_initial_defence = mock_initial_defence + c.lower()
+            odd = not odd
+        cols = st.columns([1, 2.5])
+        with cols[1]:
+            chat = st.empty()
+            with chat:
+                st.write_stream(
+                    type_writer(
+                        phrases=[
+                            ".....",
+                            0.5,
+                            f"{mock_initial_defence} seriously?",
+                            1,
+                            "  \nYou really think you can win with that?",
+                            1.5,
+                            # "  \nThe audacity",
+                            # 0.5,
+                            # "  \nThe gall",
+                            # 0.5,
+                            # "  \nThe gumption",
+                            # 0.5,
+                            # "  \nThe nerve",
+                            # 0.5,
+                            # "  \nThe temerity",
+                            # 0.5,
+                            # "  \nThe chutzpah",
+                            # 0.5,
+                            # "  \nThe brass",
+                            # 0.5,
+                            # "  \nThe effrontery",
+                            # 0.5,
+                            # "  \nThe impudence",
+                            # 0.5,
+                            # "  \nThe insolence",
+                            # 0.5,
+                            # "  \nThe impertinence",
+                            # 0.5,
+                            # "  \nThe rudeness",
+                            # 0.5,
+                            # "  \nThe presumption",
+                            # 0.5,
+                            # "  \nThe cheek",
+                        ]
+                    )
+                )
+        with cols[0]:
+            st.image(
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqPP0EROzzr9W643kfKswLqmFuNfbFA4pDWA&s",
+                width=200,
+                caption="BOOM! 💥",
+            )
+        with cols[1]:
+            with chat:
+                st.write_stream(
+                    type_writer(
+                        phrases=[
+                            "  \nTO TALK BACK ! ! !",
+                            2,
+                            "  \nI'll be representing this poor boy you siren!",
+                            2,
+                        ]
+                    )
+                )
+    if st.button(label="Start Argumentation", key="Clicked Start Argumentation"):
+        st.session_state["PHASE"] = "Argumentation"
+        st.rerun()
 
-MzMochi's defense against the charges is: {st.session_state["her_defence"]}
-
-Now give a counter argument to make her concede her case.
-
-After three turns show her evidence that she did love Saran""",
+elif st.session_state["PHASE"] == "Argumentation":
+    for message in st.session_state.messages:
+        if isinstance(message, HumanMessage):
+            with st.chat_message("user"):
+                st.markdown(message.content)
+        elif isinstance(message, AIMessage):
+            with st.chat_message("ai"):
+                st.markdown(message.content)
+    if prompt := st.chat_input("SPEAK UP BITCH!!!"):
+        # Display user message in chat message container
+        with st.chat_message("user"):
+            st.markdown(prompt)
+        # Add user message to chat history
+        st.session_state.messages.append(HumanMessage(content=prompt))
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        response = st.write_stream(
+            stream_response(
+                messages=st.session_state.messages,
+                initial_defence=st.session_state.initial_defence_value,
+                thread_id=st.session_state.thread_id,
+            )
         )
-    ]
-    config = {"thread_id": st.session_state["thread_id"]}
-
-    async def stream_response():
-        async for event in agent.astream_events(
-            input={"messages": agent_input},
-            config=config,
-            stream_mode="messages",
-            version="v2",
-        ):
-            if event["event"] == "on_chat_model_stream" and "chunk" in event["data"]:
-                yield event["data"]["chunk"].content
-
-    st.write_stream(stream_response())
+    st.session_state.messages.append(AIMessage(content=response))
